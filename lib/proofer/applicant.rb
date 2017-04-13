87c2635267cd6892bc966b26385c58231c772f3f
Peter Karman <peter.karman@gsa.gov>
2017-04-13 15:05:06 -0500

    Add Applicant.to_hash method
    **WHY**: Convenience for serialization.

--- lib/proofer/applicant.rb
+++ lib/proofer/applicant.rb
@@ -15,5 +15,11 @@ module Proofer
         instance_variable_set("@#{attr}", value)
       end
     end
+
+    def to_hash
+      instance_variables.each_with_object({}) do |var, hash|
+        hash[var.to_s.delete('@')] = instance_variable_get(var)
+      end
+    end
   end
 end
